f4613eca21f26c1128af5d33a77e2aeb3011f68e
mantlebee <mantlebee@outlook.com>
2022-06-13 20:07:44 +0200

    extractRandomItems

--- src/__tests__/arrays.test.ts
+++ src/__tests__/arrays.test.ts
@@ -1,14 +1,16 @@
 import { List } from "@mantlebee/ts-core";
 
-import { extractRandomItem } from "../arrays";
+import { extractRandomItem, extractRandomItems } from "../arrays";
 
 type Item = { id: number; name: string };
 
 const items: List<Item> = [
   { id: 1, name: "John" },
   { id: 2, name: "Jane" },
+  { id: 3, name: "Josh" },
+  { id: 4, name: "Jasper" },
 ];
-const defaultItem: Item = { id: 3, name: "Jack" };
+const defaultItem: Item = { id: 5, name: "Jack" };
 
 describe("common", () => {
   describe("utils", () => {
@@ -29,8 +31,29 @@ describe("common", () => {
         it("If extractPhysically is true, the extracted item will not be in the list anymore", () => {
           const list = [...items];
           const random = extractRandomItem(list, true);
-          expect(list.length).toBe(1);
           expect(list.find((a) => a === random)).toBeUndefined();
+        });
+      });
+      describe("extractRandomItems", () => {
+        it("Extract a random sub-list from a list", () => {
+          const randoms: List<List<Item>> = [];
+          for (let i = 0; i < 100; i++) randoms.push(extractRandomItems(items));
+          expect(randoms.some((a) => a.length < items.length)).toBeTruthy();
+        });
+        it("If list has 1 items only, the same list is returned", () => {
+          const randoms: List<List<Item>> = [];
+          for (let i = 0; i < 100; i++)
+            randoms.push(extractRandomItems([defaultItem]));
+          randoms.forEach((a) => {
+            expect(a).toEqual([defaultItem]);
+          });
+        });
+        it("If extractPhysically is true, the extracted items will not be in the list anymore", () => {
+          const list = [...items];
+          const randoms = extractRandomItems(list, true);
+          randoms.forEach((random) => {
+            expect(list.find((a) => a === random)).toBeUndefined();
+          });
         });
       });
     });
--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,7 @@ import { generateRandomNumber } from "./numbers";
 /**
  * Extracts a random item from a list of items. The extracted item can be removed by the array.
  * @param items Items from which to extract a random one.
- * @param extractPhysically If remove physically the random item from the list.
+ * @param extractPhysically If true, the random item is removed from the list.
  * @returns a random item from a list.
  */
 export function extractRandomItem<TItem>(
@@ -16,4 +16,25 @@ export function extractRandomItem<TItem>(
   const index = generateRandomNumber(items.length - 1);
   if (extractPhysically) return items.splice(index, 1)[0];
   return items[index];
+}
+
+/**
+ * Extracts a sub-list of random count a list of items. The extracted items can be removed by the array.
+ * @param items Items from which to extract a sub-list.
+ * @param extractPhysically If true, the random items are removed from the list.
+ * @returns a sub-list of random items.
+ */
+export function extractRandomItems<TItem>(
+  items: List<TItem>,
+  extractPhysically = false
+): List<TItem> {
+  if (items.length === 1) {
+    if (extractPhysically) return items.splice(0, 1);
+    return items;
+  }
+  const randomCount = generateRandomNumber(items.length - 1);
+  const randomItems: List<TItem> = [];
+  for (let i = 0; i < randomCount; ++i)
+    randomItems.push(extractRandomItem(items, extractPhysically));
+  return randomItems;
 }
