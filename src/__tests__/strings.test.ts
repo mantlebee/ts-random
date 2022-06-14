bc1ca595bd46d04383ad785622fbf71732d492ff
mantlebee <mantlebee@outlook.com>
2022-06-14 09:10:29 +0200

    generateRandomStringFromPattern

--- src/__tests__/strings.test.ts
+++ src/__tests__/strings.test.ts
@@ -4,25 +4,68 @@ import {
   getUppercaseChars,
 } from "@mantlebee/ts-core";
 
-import { generateRandomString } from "../strings";
+import {
+  generateRandomStringFromChars,
+  generateRandomStringFromPattern,
+} from "../strings";
 
 describe("common", () => {
   describe("utils", () => {
     describe("strings", () => {
-      describe("generateRandomString", () => {
+      describe("generateRandomStringFromChars", () => {
         it("Generates `aaa`", () => {
-          const random = generateRandomString("a", 3);
+          const random = generateRandomStringFromChars("a", 3);
           expect(random).toBe("aaa");
         });
         it("Generates a string of 5 numbers", () => {
           const chars = getNumberChars();
-          const random = generateRandomString(chars, 5);
+          const random = generateRandomStringFromChars(chars, 5);
           expect(/^[0-9]{5}$/.test(random)).toBeTruthy();
         });
         it("Generates a string of 10 letters, upper and lower case", () => {
           const chars = getLowercaseChars() + getUppercaseChars();
-          const random = generateRandomString(chars, 10);
+          const random = generateRandomStringFromChars(chars, 10);
           expect(/^([a-z]|[A-Z]){10}$/.test(random)).toBeTruthy();
+        });
+      });
+      describe("generateRandomStringFromPattern", () => {
+        it("Generates a random phone number like +XXX-XXXXX", () => {
+          const patterns = ["+000-00000", "+0{3}-0{5}"];
+          patterns.forEach((a) => {
+            const random = generateRandomStringFromPattern(a);
+            expect(random).toMatch(/^\+\d{3}-\d{5}$/);
+          });
+        });
+        it("Generates a random 5 chars lowercase string with first letter uppercase", () => {
+          const patterns = ["Aaaaa", "Aa{4}"];
+          patterns.forEach((a) => {
+            const random = generateRandomStringFromPattern(a);
+            expect(random).toMatch(/^[A-Z][a-z]{4}$/);
+          });
+        });
+        it("Generates a random lowercase string with first letter uppercase; length is variable from 3 to 5", () => {
+          const pattern = "Aa{3,5}";
+          const random = generateRandomStringFromPattern(pattern);
+          expect(random).toMatch(/^[A-Z][a-z]{3,5}$/);
+        });
+        it("Generates a random credit card compliant string", () => {
+          const patterns = ["0000-0000-0000-0000", "0{4}-0{4}-0{4}-0{4}"];
+          patterns.forEach((a) => {
+            const random = generateRandomStringFromPattern(a);
+            expect(random).toMatch(/^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/);
+          });
+        });
+        it("Generates a random italian IBAN compliant string", () => {
+          //IT60X0542811101000000123456
+          const pattern = "(IT)00A0000000000000000000000";
+          const random = generateRandomStringFromPattern(pattern);
+          expect(random).toMatch(/^IT[0-9]{2}[A-Z][0-9]{22}$/);
+        });
+        it("Generates a random austrian IBAN compliant string", () => {
+          // AT611904300234573201
+          const pattern = "(AT)000000000000000000";
+          const random = generateRandomStringFromPattern(pattern);
+          expect(random).toMatch(/^AT[0-9]{18}$/);
         });
       });
     });
--- src/strings.ts
+++ src/strings.ts
@@ -1,13 +1,69 @@
+import {
+  getLowercaseChars,
+  getNumberChars,
+  getUppercaseChars,
+} from "@mantlebee/ts-core";
+import { generateRandomNumber } from "./numbers";
+
 /**
  * Generates a random string from the given chars and of the given length.
  * @param chars List of chars from which to generate the string.
  * @param length Length of the string to generate.
  * @returns a random string.
  */
-export function generateRandomString(chars: string, length: number): string {
+export function generateRandomStringFromChars(
+  chars: string,
+  length: number
+): string {
   let str = "";
   for (let i = 0; i < length; i++) {
     str += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return str;
+}
+
+export function generateRandomStringFromPattern(pattern: string): string {
+  const chars = pattern.split("");
+  let skipReplace = false;
+  let muliplier = 1;
+  chars.forEach((char, index) => {
+    // (, {
+    if (/\(|\{/.test(char)) skipReplace = true;
+    // ), }
+    else if (/\)|\}/.test(char)) skipReplace = false;
+    // a, A, 0
+    else if (/[aA0]/.test(char) && !skipReplace) {
+      const nextChars = chars.slice(index + 1).join("");
+      if (/^\{\d\}/.test(nextChars)) {
+        muliplier = parseInt(chars[index + 2]);
+      }
+      if (/^\{\d,\d\}/.test(nextChars)) {
+        const max = parseInt(chars[index + 4]);
+        const min = parseInt(chars[index + 2]);
+        muliplier = generateRandomNumber(max, min);
+      }
+      let replaceWith = "";
+      for (let i = 0; i < muliplier; ++i)
+        switch (char) {
+          case "a":
+            replaceWith += generateRandomStringFromChars(
+              getLowercaseChars(),
+              1
+            );
+            break;
+          case "A":
+            replaceWith += generateRandomStringFromChars(
+              getUppercaseChars(),
+              1
+            );
+            break;
+          case "0":
+            replaceWith += generateRandomStringFromChars(getNumberChars(), 1);
+            break;
+        }
+      chars[index] = replaceWith;
+      muliplier = 1;
+    }
+  });
+  return chars.join("").replace(/\(|\)|\{\d\}|\{\d,\d\}/g, "");
 }
