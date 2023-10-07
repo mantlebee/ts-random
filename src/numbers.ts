91954734ef97e423f454de83fb4dc8295cdbec54
mantlebee <mantlebee@outlook.com>
2023-10-07 18:22:25 +0000

    Added missing comments

--- src/numbers.ts
+++ src/numbers.ts
@@ -2,6 +2,15 @@
  * Generates a random integer/float number between the given min and max values, max included.
  * To return an integer leave `decimals` to 0.
  * If `decimals` is greater than 0, the max value will be an integer anyway.
+ * @example
+ * // Generates a random number between 0 and 10.
+ * generateRandomNumber(10)
+ * @example
+ * // Generates a random number between 1 and 10.
+ * generateRandomNumber(10, 1)
+ * @example
+ * // Generates a random number between 0 and 1.
+ * generateRandomNumber(0, 1, 2)
  * @param max Maximum number that can be generated.
  * @param min Minimum number that can be generated.
  * @param decimals Digits after the decimals point.
--- src/strings.ts
+++ src/strings.ts
@@ -3,13 +3,17 @@ import {
   getNumberChars,
   getUppercaseChars,
 } from "@mantlebee/ts-core";
+
 import { generateRandomNumber } from "./numbers";
 
 /**
  * Generates a random string from the given chars and of the given length.
+ * @example
+ * // Generates a random string of 5 chars chosen between X and Y.
+ * generateRandomStringFromChars("XY", 5)
  * @param chars List of chars from which to generate the string.
  * @param length Length of the string to generate.
- * @returns a random string.
+ * @returns a random string from the given chars and of the given length.
  */
 export function generateRandomStringFromChars(
   chars: string,
@@ -22,29 +26,62 @@ export function generateRandomStringFromChars(
   return str;
 }
 
+/**
+ * Generates a random string from the given pattern. The length of the string depends on pattern itself.
+ * Patterns allow to generate strings like telephone numbers, credit card numbers, banking IBAN, ecc.
+ * Patterns are defined by special chars, escape brackets and repeater brackets:
+ *    Special Chars:
+ *      - A     : random uppercase letter
+ *      - a     : random lowercase letter
+ *      - 0     : random integer number
+ *    Escape brackets:
+ *      - ()    : chars inside are not considered special chars
+ *    Repeater brackets:
+ *      - {n}   : repeats previous special char n-1 times
+ *      - {n,M} : repeats previous special char between n and M times minus one
+ * @example
+ * // Generates a random telephone number.
+ * generateRandomStringFromPattern("+000-00000") // or "+0{3}-0{5}"
+ * @example
+ * // Generates a random credit card number.
+ * generateRandomStringFromPattern("0000-0000-0000-0000") // or "0{4}-0{4}-0{4}-0{4}"
+ * @example
+ * // Generates a random italian IBAN compliant string.
+ * generateRandomStringFromPattern("(IT)00A0{22}")
+ * @example
+ * // Generates a random lowercase string with first letter uppercase and length variable between 8 and 12 chars.
+ * generateRandomStringFromPattern("Aa{8,12}")
+ * @param pattern Pattern for the random string.
+ * @returns a random string from the given pattern.
+ */
 export function generateRandomStringFromPattern(pattern: string): string {
   const chars = pattern.split("");
   let skipReplace = false;
-  let muliplier = 1;
+  let repeats = 1;
   chars.forEach((char, index) => {
     // (, {
+      // if open bracket, skips special chars replacement
     if (/\(|\{/.test(char)) skipReplace = true;
     // ), }
+    // if close bracket, returns to replace special chars
     else if (/\)|\}/.test(char)) skipReplace = false;
     // a, A, 0
+    // if special chars, replaces it (and repeats it, if next chars are Repeater brackets)
     else if (/[aA0]/.test(char) && !skipReplace) {
+      // get next chars to update the repeats variable
       const nextChars = chars.slice(index + 1).join("");
-      if (/^\{\d*\}/.test(nextChars)) {
-        muliplier = parseInt(nextChars.replace(/\{|\}/g, ""));
-      }
+      // {n,M}
       if (/^\{\d*,\d*\}/.test(nextChars)) {
         const range = nextChars.replace(/\{|\}/g, "").split(",");
         const max = parseInt(range[0]);
         const min = parseInt(range[1]);
-        muliplier = generateRandomNumber(max, min);
-      }
+        repeats = generateRandomNumber(max, min);
+      } // {n}
+      else if (/^\{\d*\}/.test(nextChars))
+        repeats = parseInt(nextChars.replace(/\{|\}/g, ""));
+      // repleaces the special char
       let replaceWith = "";
-      for (let i = 0; i < muliplier; ++i)
+      for (let i = 0; i < repeats; ++i)
         switch (char) {
           case "a":
             replaceWith += generateRandomStringFromChars(
@@ -63,7 +100,7 @@ export function generateRandomStringFromPattern(pattern: string): string {
             break;
         }
       chars[index] = replaceWith;
-      muliplier = 1;
+      repeats = 1;
     }
   });
   return chars.join("").replace(/\(|\)|\{\d*\}|\{\d*,\d*\}/g, "");
